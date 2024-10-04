efd174792d28fe6fca30988ffbd3f59684561101
Thomas Chaigneau <chainyo@mbp-chainyo.local>
2024-10-04 16:21:42 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { ModeWatcher } from 'mode-watcher';
+	import { Toaster } from '$lib/components/ui/sonner';
+	import '../app.css';
+</script>
+
+<Toaster theme="dark" />
+<ModeWatcher defaultMode={'dark'} />
+
+<div class="bg-background relative flex min-h-screen flex-col" id="page" data-vaul-drawer-wrapper>
+	<slot />
+</div>
